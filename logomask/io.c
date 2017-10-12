cbc9fabc41cfb632aa05ce5b32794c757c4a76cf
vanhofen <vanhofen@gmx.de>
2017-10-12 13:14:07 +0200

    - try to fix rc input devices; use neutrino's config.h

--- logomask/io.c
+++ logomask/io.c
@@ -15,10 +15,8 @@
 #include <sys/dir.h>
 #include <sys/stat.h>
 #include <linux/input.h>
-#include <poll.h>
 
 #include "io.h"
-#include "tuxwetter.h"
 
 extern int instance;
 struct input_event ev;
@@ -30,7 +28,7 @@ int InitRC(void)
 	rc = open(RC_DEVICE, O_RDONLY | O_CLOEXEC);
 	if(rc == -1)
 	{
-		perror("tuxwetter <open remote control>");
+		perror("msgbox <open remote control>");
 		exit(1);
 	}
 	fcntl(rc, F_SETFL, O_NONBLOCK | O_SYNC);
@@ -59,66 +57,19 @@ int RCKeyPressed(void)
 	return 0;
 }
 
-void ClearRC(void)
+
+int GetRCCode(void)
 {
-	struct pollfd pfd;
-	pfd.fd = rc;
-	pfd.events = POLLIN;
-	pfd.revents = 0;
-
-	do
-		poll(&pfd, 1, 300);
-	while(read(rc, &ev, sizeof(ev)) == sizeof(ev));
-}
-
-
-int GetRCCode(int timeout_in_ms)
-{
-	int rv = -1;
-
-	if (get_instance()>instance)
+	int rv;
+	
+	if(!RCKeyPressed())
 	{
-		return rv;
+		return -1;
 	}
-
-	if (timeout_in_ms) {
-		struct pollfd pfd;
-		struct timeval tv;
-		uint64_t ms_now, ms_final;
-
-		pfd.fd = rc;
-		pfd.events = POLLIN;
-		pfd.revents = 0;
-
-		gettimeofday( &tv, NULL );
-		ms_now = tv.tv_usec/1000 + tv.tv_sec * 1000;
-		if (timeout_in_ms > 0)
-			ms_final = ms_now + timeout_in_ms;
-		else
-			ms_final = UINT64_MAX;
-		while (ms_final > ms_now) {
-			switch(poll(&pfd, 1, timeout_in_ms)) {
-				case -1:
-					perror("GetRCCode: poll() failed");
-				case 0:
-					return -1;
-				default:
-					;
-			}
-			if(RCKeyPressed()) {
-				rv = rccode;
-				while(RCKeyPressed());
-				return rv;
-			}
-
-			gettimeofday( &tv, NULL );
-			ms_now = tv.tv_usec/1000 + tv.tv_sec * 1000;
-			if (timeout_in_ms > 0)
-				timeout_in_ms = (int)(ms_final - ms_now);
-		}
-	} else if(RCKeyPressed()) {
-		rv = rccode;
-		while(RCKeyPressed());
-	}
+	rv=rccode;
+//	while(RCKeyPressed());
+	
 	return rv;
 }
+
+
