3f6ab17a5d324549296650c29338f58d521cb2cf
max_10 <max_10@gmx.de>
2018-10-17 02:07:54 +0200

    fix warning: redefined

--- tuxwetter/tuxwetter.h
+++ tuxwetter/tuxwetter.h
@@ -28,6 +28,13 @@
 #include <config.h>
 #endif
 
+#ifndef FB_DEVICE
+#define FB_DEVICE	"/dev/fb/0"
+#endif
+#ifndef FB_DEVICE_FALLBACK
+#define FB_DEVICE_FALLBACK	"/dev/fb0"
+#endif
+
 #ifndef CONFIGDIR
 #define CONFIGDIR "/var/tuxbox/config"
 #endif
